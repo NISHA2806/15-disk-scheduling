c2489e954d6d1b83bfa5dc992843658e0f693692
NISHANTHINI S <astronisha2806@gmail.com>
2026-08-17 08:15:06 +0530

    Implement SSTF disk scheduling algorithm

---
+++ sstf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+queue=(98 183 37 122 14 124 65 67)
+head=53
+seek=0
+
+echo "SSTF Disk Scheduling"
+
+for req in "${queue[@]}"
+do
+    diff=$((req-head))
+
+    if [ $diff -lt 0 ]
+    then
+        diff=$((-diff))
+    fi
+
+    seek=$((seek+diff))
+    head=$req
+done
+
+echo "Total Head Movement = $seek"
