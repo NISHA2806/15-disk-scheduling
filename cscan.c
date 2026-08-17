312cc6451a2d1e1a332aaa286d07f82e53ca554f
NISHANTHINI S <astronisha2806@gmail.com>
2026-08-17 08:16:06 +0530

    Create cscan.c

---
+++ cscan.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int req[20], n, head, disk, i, j, temp;
+    int seek = 0;
+
+    printf("Enter Number of Requests: ");
+    scanf("%d", &n);
+
+    printf("Enter Request Queue:\n");
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &req[i]);
+
+    printf("Enter Initial Head Position: ");
+    scanf("%d", &head);
+
+    printf("Enter Disk Size: ");
+    scanf("%d", &disk);
+
+    for(i = 0; i < n - 1; i++)
+    {
+        for(j = i + 1; j < n; j++)
+        {
+            if(req[i] > req[j])
+            {
+                temp = req[i];
+                req[i] = req[j];
+                req[j] = temp;
+            }
+        }
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(req[i] >= head)
+        {
+            seek += abs(req[i] - head);
+            head = req[i];
+        }
+    }
+
+    seek += (disk - 1) - head;
+    head = 0;
+
+    seek += disk - 1;
+
+    for(i = 0; i < n; i++)
+    {
+        if(req[i] < head)
+        {
+            seek += abs(req[i] - head);
+            head = req[i];
+        }
+    }
+
+    printf("Total Head Movement = %d\n", seek);
+
+    return 0;
+}
